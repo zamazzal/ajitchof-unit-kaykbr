b549ddba89ecffcd6876dd8bb7fce49425a764a0
Ilyass Shaimoun <ishaimou@e1r3p15.1337.ma>
2018-12-02 04:09:55 +0100

    add main.c

---
+++ modifyed-by-zamazzal/tests/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ishaimou <ilyass.sh@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/02 04:06:12 by ishaimou          #+#    #+#             */
+/*   Updated: 2018/12/02 04:07:35 by ishaimou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void main(void)
+{
+	atoi_launcher();
+	isalnum_launcher();
+	isalpha_launcher();
+	isascii_launcher();
+	isdigit_launcher();
+	isprint_launcher();
+	strchr_launcher();
+	strdup_launcher();
+	strlen_launcher();
+	strncat_launcher();
+	strncmp_launcher();
+	strncpy_launcher();
+	strnstr_launcher();
+	strrchr_launcher();
+	strstr_launcher();
+	tolower_launcher();
+	toupper_launcher();
+}
